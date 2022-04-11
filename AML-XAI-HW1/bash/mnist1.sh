42f581213efe56a1e2b121df246c34ed3746c620
olccihyeon <sh102201@snu.ac.kr>
2022-04-11 12:38:45 +0900

    ewc 수정

--- AML-XAI-HW1/bash/mnist1.sh
+++ AML-XAI-HW1/bash/mnist1.sh
@@ -3,6 +3,6 @@ for TRAIN in "l2" "ewc"
 do
 for LAMB in 0.001 0.01 0.1 1 10 40 100 400 700 1000
 do
-echo python3 main.py --trainer $TRAIN --dataset MNIST --nepochs 20 --lr 0.001 --lamb $LAMB
+    	python3 main.py --trainer $TRAIN --dataset MNIST --nepochs 20 --lr 0.001 --lamb $LAMB
 done
 done
